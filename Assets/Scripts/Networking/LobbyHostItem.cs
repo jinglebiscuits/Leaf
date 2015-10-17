17eabe7bec83e656ee3f292a372f4c489c46e3ef
jinglebiscuits <scott.wehby@gmail.com>
2015-10-17 10:45:31 -0500

    getting a personal scene set up. Note that I commented out some networking scripts in order to build the project.

--- Assets/Scripts/Networking/LobbyHostItem.cs
+++ Assets/Scripts/Networking/LobbyHostItem.cs
@@ -1,32 +1,32 @@
-﻿using UnityEngine;
-using UnityEngine.UI;
-using System.Collections;
+﻿//  using UnityEngine;
+//  using UnityEngine.UI;
+//  using System.Collections;
 
-public class LobbyHostItem : MonoBehaviour {
+//  public class LobbyHostItem : MonoBehaviour {
 
-    public string IPAddress;
-    public int Port;
+//      public string IPAddress;
+//      public int Port;
 
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="strIpAddress"></param>
-    /// <param name="intPort"></param>
-    public void Init(string strIpAddress, int intPort)
-    {
-        this.IPAddress = strIpAddress;
-        this.Port = intPort;
-        //get the text box, there is only one
-        Text txt = GetComponentInChildren<Text>();
-        //set it to the ip address
-        txt.text = strIpAddress;
-    }
+//      /// <summary>
+//      /// 
+//      /// </summary>
+//      /// <param name="strIpAddress"></param>
+//      /// <param name="intPort"></param>
+//      public void Init(string strIpAddress, int intPort)
+//      {
+//          this.IPAddress = strIpAddress;
+//          this.Port = intPort;
+//          //get the text box, there is only one
+//          Text txt = GetComponentInChildren<Text>();
+//          //set it to the ip address
+//          txt.text = strIpAddress;
+//      }
 
-    /// <summary>
-    /// 
-    /// </summary>
-    public void SelectButtonClickHandler()
-    {
-        LobbyUI.instace.HostSelected(IPAddress, Port);
-    }
-}
+//      /// <summary>
+//      /// 
+//      /// </summary>
+//      public void SelectButtonClickHandler()
+//      {
+//          LobbyUI.instace.HostSelected(IPAddress, Port);
+//      }
+//  }
--- Assets/Scripts/Networking/NetworkUtils.cs
+++ Assets/Scripts/Networking/NetworkUtils.cs
@@ -1,70 +1,70 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using UnityEngine.Networking;
-using UnityEngine;
+﻿//  using System;
+//  using System.Collections.Generic;
+//  using System.Linq;
+//  using System.Text;
+//  using UnityEngine.Networking;
+//  using UnityEngine;
 
-/// <summary>
-/// 
-/// </summary>
-public class NetworkUtils
-{
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="bytes"></param>
-    /// <returns></returns>
-    public static string BytesToString(byte[] bytes)
-    {
-        char[] chrArray = new char[(int)bytes.Length / 2];
-        Buffer.BlockCopy(bytes, 0, chrArray, 0, (int)bytes.Length);
-        return new string(chrArray);
-    }
+//  /// <summary>
+//  /// 
+//  /// </summary>
+//  public class NetworkUtils
+//  {
+//      /// <summary>
+//      /// 
+//      /// </summary>
+//      /// <param name="bytes"></param>
+//      /// <returns></returns>
+//      public static string BytesToString(byte[] bytes)
+//      {
+//          char[] chrArray = new char[(int)bytes.Length / 2];
+//          Buffer.BlockCopy(bytes, 0, chrArray, 0, (int)bytes.Length);
+//          return new string(chrArray);
+//      }
 
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="str"></param>
-    /// <returns></returns>
-    public static byte[] StringToBytes(string str)
-    {
-        byte[] numArray = new byte[str.Length * 2];
-        Buffer.BlockCopy(str.ToCharArray(), 0, numArray, 0, (int)numArray.Length);
-        return numArray;
-    }
+//      /// <summary>
+//      /// 
+//      /// </summary>
+//      /// <param name="str"></param>
+//      /// <returns></returns>
+//      public static byte[] StringToBytes(string str)
+//      {
+//          byte[] numArray = new byte[str.Length * 2];
+//          Buffer.BlockCopy(str.ToCharArray(), 0, numArray, 0, (int)numArray.Length);
+//          return numArray;
+//      }
 
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="strIp"></param>
-    /// <returns></returns>
-    public static string ParseIPFromNetworkBroadcastResult(string strIp)
-    {
-        //the forlat looks like this
-        //::ffff:192.168.56.1
-        //we should split by : and return the last element
-        string[] strParts = strIp.Split(':');
-        string strRetVal = strParts[strParts.Length - 1];
-        return strRetVal;
-    }
+//      /// <summary>
+//      /// 
+//      /// </summary>
+//      /// <param name="strIp"></param>
+//      /// <returns></returns>
+//      public static string ParseIPFromNetworkBroadcastResult(string strIp)
+//      {
+//          //the forlat looks like this
+//          //::ffff:192.168.56.1
+//          //we should split by : and return the last element
+//          string[] strParts = strIp.Split(':');
+//          string strRetVal = strParts[strParts.Length - 1];
+//          return strRetVal;
+//      }
 
-    public static bool GetSocketInfoFromNetworkBroadcastResult(ref NetworkBroadcastResult item, ref string ipAddress, ref int port)
-    {
-        bool blnRetVal = true;
-        string str = NetworkUtils.BytesToString(item.broadcastData);
-        string[] strArrays = str.Split(new char[] { ':' });
-        if ((int)strArrays.Length == 3)
-        {
-            ipAddress = NetworkUtils.ParseIPFromNetworkBroadcastResult(item.serverAddress);
-            port = Convert.ToInt32(strArrays[2]);
-        }
-        else
-        {
-            Debug.Log("GetSocketInfoFromNetworkBroadcastResult - bad data format");
-        }
+//      public static bool GetSocketInfoFromNetworkBroadcastResult(ref NetworkBroadcastResult item, ref string ipAddress, ref int port)
+//      {
+//          bool blnRetVal = true;
+//          string str = NetworkUtils.BytesToString(item.broadcastData);
+//          string[] strArrays = str.Split(new char[] { ':' });
+//          if ((int)strArrays.Length == 3)
+//          {
+//              ipAddress = NetworkUtils.ParseIPFromNetworkBroadcastResult(item.serverAddress);
+//              port = Convert.ToInt32(strArrays[2]);
+//          }
+//          else
+//          {
+//              Debug.Log("GetSocketInfoFromNetworkBroadcastResult - bad data format");
+//          }
 
-        return blnRetVal;
-    }
-}
+//          return blnRetVal;
+//      }
+//  }
 
