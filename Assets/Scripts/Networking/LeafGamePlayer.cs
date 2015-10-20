ee818d78a5246c94fea07b9ef75f608f967a1cc5
Mike Brascome <brascome@uab.edu>
2015-10-20 13:40:00 -0500

    started game player networking code

--- Assets/Scripts/Networking/LeafGamePlayer.cs
+++ Assets/Scripts/Networking/LeafGamePlayer.cs
@@ -1,8 +1,11 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class LeafGamePlayer : MonoBehaviour {
 
+    public Text txtRedScore;
+    public Text txtBlueScore;
     public int score { get; set; }
     public Constants.PlayerColors playerColor { get; set; }
 
