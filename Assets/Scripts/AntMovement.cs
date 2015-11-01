3f0bed6c1d822c37f3255fedf6eda831edf8adc4
jinglebiscuits <scott.wehby@gmail.com>
2015-11-01 12:42:21 -0500

    added movement script to get that son of a bitch working

---
+++ Assets/Scripts/AntMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AntMovement : MonoBehaviour {
+
+	public float moveSpeed = 0.5f;
+	public bool moving = false;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (moving) {
+			transform.Translate(Vector2.up * Time.deltaTime * moveSpeed);
+		}
+	}
+	
+	public void Turn() {
+		transform.Rotate(0, 0, 90);
+	}
+}
